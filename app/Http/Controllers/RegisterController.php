8cf56de0de5de857aa4ca771f0aef8b3731c185b
Mustofa Akhyar <165134256+MustofaAkhyar@users.noreply.github.com>
2024-12-23 22:37:36 +0700

    Update RegisterController.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -40,7 +40,7 @@ class RegisterController extends Controller
                 'success' => true,
                 'message' => 'User berhasil didaftarkan',
                 'data' => $success
-            ], 200);
+            ], Response::HTTP_CREATED);
         }
     }
 
